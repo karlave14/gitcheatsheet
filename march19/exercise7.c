a3be9ac944c995f9a62edda6c5b5787789745e6a
karlave14 <karla_valdeze14@hotmail.com>
2020-03-20 08:32:13 -0600

    Exercise 8, 9 and 10

--- march19/exercise7.c
+++ march19/exercise7.c
@@ -5,7 +5,8 @@ int main(int argc, char *argv[])
 	int bugs = 100;
 	double bug_rate = 1.2;
 	printf("You have %d bugs at the imaginary rate of %f.\n", bugs, bug_rate);
-	long universe_of_defects = 1L * 1024L * 1024L * 1024L;
+	long universe_of_defects = 1024L * 1024L * 1024L * 1024L * 1024L * 1024L * 1024L;
+    //When add five 1024L can show error of warning
 	printf("The entire universe has %ld bugs.\n", universe_of_defects);
 	double expected_bugs = bugs * bug_rate;
 	printf("You are expected to have %f bugs.\n", expected_bugs);
